b9a8a1e58fc522a9e0a1653b28925546af1af782
lavallehaus <kiabethmgmt@gmail.com>
2026-06-01 13:19:57 -0700

    Update data.js

--- api/data.js
+++ api/data.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     await fetch(`${url}/set/lavalle_data`, {
       method: "POST",
       headers: { Authorization: `Bearer ${token}`, "Content-Type": "application/json" },
-      body: JSON.stringify([body])
+      body: JSON.stringify(body)
     });
     res.json({ ok: true });
   }
